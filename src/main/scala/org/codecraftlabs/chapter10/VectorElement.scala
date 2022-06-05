31bfd78bb71e236ead504da7d84431df836b57b8
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2022-06-05 21:36:33 +0200

    more code

--- src/main/scala/org/codecraftlabs/chapter10/VectorElement.scala
+++ src/main/scala/org/codecraftlabs/chapter10/VectorElement.scala
@@ -1,6 +1,3 @@
 package org.codecraftlabs.chapter10
 
-class VectorElement(conts: Vector[String]) extends Element {
-  private val elementContents: Vector[String] = conts
-  def contents: Vector[String] = elementContents
-}
+class VectorElement(val contents: Vector[String]) extends Element
