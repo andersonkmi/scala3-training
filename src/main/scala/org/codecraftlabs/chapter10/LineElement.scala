50d84400f298e9638c7a778ad564cd73ae33eb95
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2022-06-09 22:56:07 +0200

    more code

--- src/main/scala/org/codecraftlabs/chapter10/LineElement.scala
+++ src/main/scala/org/codecraftlabs/chapter10/LineElement.scala
@@ -1,6 +1,7 @@
 package org.codecraftlabs.chapter10
 
-class LineElement (val s: String) extends VectorElement (Vector(s)) {
+class LineElement (val s: String) extends Element {
+  val contents: Vector[String] = Vector(s)
   override def width: Int = s.length
   override def height: Int = 1
 }
