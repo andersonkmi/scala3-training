e716aa78c26fbe56663186a3ea1833cde61c645c
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2022-06-09 23:09:37 +0200

    more code

--- src/main/scala/org/codecraftlabs/chapter10/Element.scala
+++ src/main/scala/org/codecraftlabs/chapter10/Element.scala
@@ -4,4 +4,17 @@ abstract class Element {
   def contents: Vector[String]
   def height: Int = contents.length
   def width: Int = if height == 0 then 0 else contents(0).length
+
+  def above(that: Element): Element = {
+    VectorElement(this.contents ++ that.contents)
+  }
+
+  def beside(that: Element): Element = {
+    val newContents = new Array[String](this.contents.length)
+
+    for i <- this.contents.indices do
+      newContents(i) = this.contents(i) + that.contents(i)
+
+    VectorElement(newContents.toVector)
+  }
 }
