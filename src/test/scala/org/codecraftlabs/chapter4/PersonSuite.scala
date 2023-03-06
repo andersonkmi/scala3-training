6e1c3d2e475b832a08490df4d4f0a346e33b1ac0
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2023-03-06 21:51:24 +0100

    One more test

--- src/test/scala/org/codecraftlabs/chapter4/PersonSuite.scala
+++ src/test/scala/org/codecraftlabs/chapter4/PersonSuite.scala
@@ -1,11 +1,18 @@
 package org.codecraftlabs.chapter4
 
-import org.codecraftlabs.Rational
-
 class PersonSuite extends munit.FunSuite {
   test("Create simple person") {
     val person1 = Person("Anderson", 32)
     assertEquals(person1.name, "Anderson")
     assertEquals(person1.age, 32)
+  }
+
+  test("Call appendToName should return a new person") {
+    val originalPerson = Person("Anderson", 32)
+    val modifiedPerson = originalPerson.appendToName("Silva")
+
+    assertNotEquals(originalPerson, modifiedPerson)
+    assertEquals(modifiedPerson.name, "Anderson Silva")
+    assertEquals(modifiedPerson.age, 32)
   }
 }
