45645662cdccbf53d24536623781f90c782203d2
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2023-03-08 23:04:12 +0100

    Padding example - local function example

---
+++ src/main/scala/org/codecraftlabs/chapter8/Padding.scala
@@ -0,0 +1,13 @@
+package org.codecraftlabs.chapter8
+
+object Padding:
+  def padLines(text: String, minWidth: Int): String =
+    def padLine(line: String): String =
+      if line.length >= minWidth then line
+      else line + " " * (minWidth - line.length)
+      
+    val paddedLines =
+      for line <- text.linesIterator yield
+        padLine(line)
+        
+    paddedLines.mkString("\n")
