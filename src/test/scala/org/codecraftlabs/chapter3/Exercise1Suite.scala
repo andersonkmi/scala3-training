15fd89d17b6aaf12ad5f090674263c957a70c3ba
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2023-03-15 23:53:53 +0100

    import

--- src/test/scala/org/codecraftlabs/chapter3/Exercise1Suite.scala
+++ src/test/scala/org/codecraftlabs/chapter3/Exercise1Suite.scala
@@ -1,14 +1,15 @@
 package org.codecraftlabs.chapter3
 
 import org.codecraftlabs.chapter2.Exercise5
+import org.codecraftlabs.chapter3.Exercise1.generateArray
 
 class Exercise1Suite extends munit.FunSuite {
   test("Generate array of size 1") {
-    assert(Exercise1.generateArray(1).length == 1)
+    assert(generateArray(1).length == 1)
   }
 
   test("Generate array of size 30") {
-    val result = Exercise1.generateArray(30)
+    val result = generateArray(30)
     assert(result.length == 30)
   }
 }
