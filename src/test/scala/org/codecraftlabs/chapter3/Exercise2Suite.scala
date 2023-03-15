9c85b7570a756dd96512f0e7289239ffd5ce0c87
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2023-03-15 23:50:37 +0100

    Import

--- src/test/scala/org/codecraftlabs/chapter3/Exercise2Suite.scala
+++ src/test/scala/org/codecraftlabs/chapter3/Exercise2Suite.scala
@@ -1,9 +1,11 @@
 package org.codecraftlabs.chapter3
 
+import org.codecraftlabs.chapter3.Exercise2.swapValues
+
 class Exercise2Suite extends munit.FunSuite {
   test("Swap values") {
     val values = Array(1, 2)
-    Exercise2.swapValues(values)
+    swapValues(values)
 
     val expected = Array(2, 1)
     assertEquals(values.mkString, expected.mkString)
@@ -11,7 +13,7 @@ class Exercise2Suite extends munit.FunSuite {
 
   test("Swap values one item") {
     val values = Array(1)
-    Exercise2.swapValues(values)
+    swapValues(values)
 
     val expected = Array(1)
     assertEquals(values.mkString, expected.mkString)
@@ -19,7 +21,7 @@ class Exercise2Suite extends munit.FunSuite {
 
   test("Swap values array with 3 values") {
     val values = Array(1, 2, 3)
-    Exercise2.swapValues(values)
+    swapValues(values)
 
     val expected = Array(2, 1, 3)
     assertEquals(values.mkString, expected.mkString)
@@ -27,7 +29,7 @@ class Exercise2Suite extends munit.FunSuite {
 
   test("Swap values array with 4 values") {
     val values = Array(1, 2, 3, 4)
-    Exercise2.swapValues(values)
+    swapValues(values)
 
     val expected = Array(2, 1, 4, 3)
     assertEquals(values.mkString, expected.mkString)
