c7d0f3f5162f10997c74bbce1fbdf86997b789ae
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2023-04-10 20:01:25 +0200

    add more tests

--- src/test/scala/org/codecraftlabs/chapter8/BankAccountSuite.scala
+++ src/test/scala/org/codecraftlabs/chapter8/BankAccountSuite.scala
@@ -1,7 +1,23 @@
 package org.codecraftlabs.chapter8
 
 class BankAccountSuite extends munit.FunSuite:
-  test("Withdraw money") {
-    val bankAccount = new BankAccount(0.00)
+  test("Create initial account") {
+    val bankAccount = BankAccount(0.00)
     assertEquals(bankAccount.currentBalance, 0.00)
+  }
+
+  test ("Deposit into account") {
+    val account = BankAccount(0.00)
+    assertEquals(account.currentBalance, 0.00)
+
+    account.deposit(1.00)
+    assertEquals(account.currentBalance, 1.00)
+  }
+
+  test("Withdraw from account") {
+    val account = BankAccount(10.00)
+    assertEquals(account.currentBalance, 10.00)
+
+    account.withdraw(1.00)
+    assertEquals(account.currentBalance, 9.00)
   }
