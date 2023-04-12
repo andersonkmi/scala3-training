ed44bfb2e5e4195871c1f813d9dd44fa12fbb875
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2023-04-13 00:00:23 +0200

    more code

--- src/test/scala/org/codecraftlabs/chapter8/BundleSuite.scala
+++ src/test/scala/org/codecraftlabs/chapter8/BundleSuite.scala
@@ -6,3 +6,21 @@ class BundleSuite extends munit.FunSuite:
     assertEquals(bundle.description, "")
     assertEquals(bundle.price, 0.00)
   }
+
+  test("Create single item bundle") {
+    val bundle = Bundle()
+    val item1 = SimpleItem(description = "Product 1", price = 1.12)
+    bundle.add(item1)
+
+    assertEquals(bundle.description, "Product 1")
+    assertEquals(bundle.price, 1.12)
+  }
+
+  test("Create multiple item bundle") {
+    val bundle = Bundle()
+    bundle.add(SimpleItem(description = "Product 1", price = 1.12))
+    bundle.add(SimpleItem(description = "Product 2", price = 2.34))
+
+    assertEquals(bundle.description, "Product 1, Product 2")
+    assertEquals(bundle.price, 3.46)
+  }
