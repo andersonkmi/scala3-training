9ee3b5b6db33ffcf0a560dee320771523a3f5ace
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2023-04-16 21:03:39 +0200

    change number format

--- src/test/scala/org/codecraftlabs/chapter8/LabeledPointSuite.scala
+++ src/test/scala/org/codecraftlabs/chapter8/LabeledPointSuite.scala
@@ -2,7 +2,7 @@ package org.codecraftlabs.chapter8
 
 class LabeledPointSuite extends munit.FunSuite:
   test("Create simple labeled point") {
-    val labeledPoint = LabeledPoint("Start point", 1, 1)
+    val labeledPoint = LabeledPoint("Start point", 1.00, 1.00)
     assertEquals(labeledPoint.label, "Start point")
     assertEquals(labeledPoint.x, 1.00)
     assertEquals(labeledPoint.y, 1.00)
