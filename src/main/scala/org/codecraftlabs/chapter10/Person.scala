f2f5a4bf8798d2e26ff6cc14b612deae9a15d062
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2023-05-01 21:37:11 +0200

    changes

--- src/main/scala/org/codecraftlabs/chapter10/Person.scala
+++ src/main/scala/org/codecraftlabs/chapter10/Person.scala
@@ -2,5 +2,6 @@ package org.codecraftlabs.chapter10
 
 
 class Person(private val _name: String) extends PrinterLogger with CryptoLogger {
+  override val key: Int = 9
   def name = {log(_name); _name}
 }
