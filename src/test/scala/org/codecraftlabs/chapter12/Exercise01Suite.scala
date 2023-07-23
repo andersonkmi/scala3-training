5c5c1a93ce215c930b9db029c066103bd842b068
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2023-07-23 23:12:47 +0200

    Add extra test

--- src/test/scala/org/codecraftlabs/chapter12/Exercise01Suite.scala
+++ src/test/scala/org/codecraftlabs/chapter12/Exercise01Suite.scala
@@ -8,4 +8,10 @@ class Exercise01Suite extends munit.FunSuite {
     val result = values(x => x * x, 1, 2)
     assertEquals(result, expectedResult)
   }
+
+  test ("Test case 2") {
+    val expectedResult: Seq[(Int, Int)] = Seq((1, 1), (2, 8), (3, 27))
+    val result = values(x => x * x * x, 1, 3)
+    assertEquals(result, expectedResult)
+  }
 }
