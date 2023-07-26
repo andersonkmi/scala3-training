b6076de64065fdf5cefc7f822f24dac458cc96f0
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2023-07-26 08:31:08 +0200

    Adjust the tests

--- src/test/scala/org/codecraftlabs/chapter12/Exercise05Suite.scala
+++ src/test/scala/org/codecraftlabs/chapter12/Exercise05Suite.scala
@@ -1,15 +1,17 @@
 package org.codecraftlabs.chapter12
 
+import org.codecraftlabs.chapter12.Exercise05.largest
+
 class Exercise05Suite extends munit.FunSuite {
   test("Test case 1") {
     val expected: Int = 25
-    val result = Exercise05.largest(x => 10 * x - x * x, 1 to 10)
+    val result = largest(x => 10 * x - x * x, 1 to 10)
     assertEquals(result, expected)
   }
 
   test("Test case 2") {
     val expected = 10
-    val result = Exercise05.largest(x => x * 1, 1 to 10)
+    val result = largest(x => x * 1, 1 to 10)
     assertEquals(result, expected)
   }
 }
--- src/test/scala/org/codecraftlabs/chapter12/Exercise06Suite.scala
+++ src/test/scala/org/codecraftlabs/chapter12/Exercise06Suite.scala
@@ -1,10 +1,17 @@
 package org.codecraftlabs.chapter12
 
+import org.codecraftlabs.chapter12.Exercise06.largestAt
+
 class Exercise06Suite extends munit.FunSuite {
   test("Test case 01") {
     val expected = 5
-    val result = Exercise06.largestAt(x => 10 * x - x * x, 1 to 10)
+    val result = largestAt(x => 10 * x - x * x, 1 to 10)
     assertEquals(result, expected)
   }
 
+  test("Test case 02") {
+    val expected = 10
+    val result = largestAt(x => x / 2, 1 to 10)
+    assertEquals(result, expected)
+  }
 }
